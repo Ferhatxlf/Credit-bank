2acc7dfc97282e2c723f27ae041911ab00b6bf8b
Ferhat Khelf <ferhat.khelf@fgei.ummto.dz>
2023-12-15 21:32:26 +0100

    ajout de la modale

--- src/app/auth/authlayout/authlayout.component.ts
+++ src/app/auth/authlayout/authlayout.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-authlayout',
   templateUrl: './authlayout.component.html',
-  styleUrl: './authlayout.component.css'
+  styleUrl: './authlayout.component.css',
 })
 export class AuthlayoutComponent {
+  constructor(private location: Location) {}
 
+  goBack(): void {
+    this.location.back();
+  }
+
+  public showModale: boolean = false;
+  public toggleModale(): any {
+    this.showModale = !this.showModale;
+  }
 }
