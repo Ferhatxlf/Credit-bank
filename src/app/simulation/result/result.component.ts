fb9621ce51667fe48b504e2b1cb9df7252070189
mahrez <mahreztouzi@gmail.com>
2023-12-17 14:28:32 +0100

    result/pdf

--- src/app/simulation/result/result.component.ts
+++ src/app/simulation/result/result.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import * as html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styleUrl: './result.component.css'
+  styleUrl: './result.component.css',
 })
 export class ResultComponent {
-
+  generatePDF() {
+    const element = document.querySelector('.pdf');
+    html2pdf(element);
+  }
 }
