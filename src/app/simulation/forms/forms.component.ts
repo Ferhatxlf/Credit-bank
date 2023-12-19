04e2713cb3b7c63ebb4cd78033a76eded29bc4f8
Ferhat Khelf <ferhat.khelf@fgei.ummto.dz>
2023-12-19 16:03:46 +0100

    my branch

--- src/app/simulation/forms/forms.component.ts
+++ src/app/simulation/forms/forms.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrl: './forms.component.css'
+  styleUrl: './forms.component.css',
 })
 export class FormsComponent {
+  public co_emprunteur: boolean = false;
+  public otherFinancement: boolean = false;
+  public patrimoine: boolean = false;
 
+  hasCoEmprunter() {
+    this.co_emprunteur = true;
+  }
+  dontHasCoEmprunter() {
+    this.co_emprunteur = false;
+  }
+
+  haspatrimoine() {
+    this.patrimoine = true;
+  }
+  dontHaspatrimoine() {
+    this.patrimoine = false;
+  }
+  hasOtherF() {
+    this.otherFinancement = true;
+  }
+
+  dontHasOtherF() {
+    this.otherFinancement = false;
+  }
 }
