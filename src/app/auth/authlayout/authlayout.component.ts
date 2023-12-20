647f7b01d0873f347365304ff2bf7a14448aff53
unknown <krimbelkacem.hamnache@fgei.ummto.dz>
2023-12-20 12:26:42 +0100

    new

--- src/app/auth/authlayout/authlayout.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-authlayout',
-  templateUrl: './authlayout.component.html',
-  styleUrl: './authlayout.component.css',
-})
-export class AuthlayoutComponent {}
