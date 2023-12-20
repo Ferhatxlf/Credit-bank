d7e67ea873773033957df2c023051444963d0f56
Ferhat Khelf <ferhat.khelf@fgei.ummto.dz>
2023-12-20 12:40:48 +0100

    dahshbord

--- src/app/simulation/forms/forms.component.ts
+++ src/app/simulation/forms/forms.component.ts
@@ -3,31 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrl: './forms.component.css',
+  styleUrl: './forms.component.css'
 })
 export class FormsComponent {
-  public co_emprunteur: boolean = false;
-  public otherFinancement: boolean = false;
-  public patrimoine: boolean = false;
 
-  hasCoEmprunter() {
-    this.co_emprunteur = true;
-  }
-  dontHasCoEmprunter() {
-    this.co_emprunteur = false;
-  }
-
-  haspatrimoine() {
-    this.patrimoine = true;
-  }
-  dontHaspatrimoine() {
-    this.patrimoine = false;
-  }
-  hasOtherF() {
-    this.otherFinancement = true;
-  }
-
-  dontHasOtherF() {
-    this.otherFinancement = false;
-  }
 }
