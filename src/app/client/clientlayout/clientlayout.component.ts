65bd3a9b51d60ffc4309a1d3a8d3e027243d2f20
Ferhat Khelf <ferhat.khelf@fgei.ummto.dz>
2023-12-20 14:13:45 +0100

    dashbord

--- src/app/client/clientlayout/clientlayout.component.ts
+++ src/app/client/clientlayout/clientlayout.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-clientlayout',
   templateUrl: './clientlayout.component.html',
-  styleUrl: './clientlayout.component.css'
+  styleUrl: './clientlayout.component.css',
 })
 export class ClientlayoutComponent {
+  constructor(private location: Location) {
+    this.url = this.location.path();
+    // Écouter les changements d'URL
+    this.location.onUrlChange((url) => {
+      this.url = url;
+      console.log(this.url);
+    });
+  }
+  url: string = '';
 
+  ngOnInit() {}
+
+  goBack(): void {
+    this.location.back();
+  }
 }
