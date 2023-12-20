b1763fbfc72278201ee3b1b88236cfc7da15c233
Ferhat Khelf <ferhat.khelf@fgei.ummto.dz>
2023-12-20 15:30:33 +0100

    qlq modification sur la side bar client

--- src/app/client/clientlayout/clientlayout.component.ts
+++ src/app/client/clientlayout/clientlayout.component.ts
@@ -7,6 +7,10 @@ import { Location } from '@angular/common';
   styleUrl: './clientlayout.component.css',
 })
 export class ClientlayoutComponent {
+  public selected: boolean = false;
+  public cselected: boolean = false;
+  public dselected: boolean = false;
+  public pselected: boolean = false;
   constructor(private location: Location) {
     this.url = this.location.path();
     // Écouter les changements d'URL
@@ -21,5 +25,33 @@ export class ClientlayoutComponent {
 
   goBack(): void {
     this.location.back();
+  }
+
+  dashboardSelected() {
+    this.selected = true;
+    this.cselected = false;
+    this.dselected = false;
+    this.pselected = false;
+  }
+
+  creditSelected() {
+    this.selected = false;
+    this.cselected = true;
+    this.dselected = false;
+    this.pselected = false;
+  }
+
+  dossierSelected() {
+    this.selected = false;
+    this.cselected = false;
+    this.dselected = true;
+    this.pselected = false;
+  }
+
+  profileSelected() {
+    this.selected = false;
+    this.cselected = false;
+    this.dselected = false;
+    this.pselected = true;
   }
 }
