063acc03b6e72e83b3b3ce26b846a262a6fbe757
Ferhat Khelf <ferhat.khelf@fgei.ummto.dz>
2023-12-30 15:05:36 +0100

    ajout des interfaces du courtier

--- src/app/courtier/courtier-routing.module.ts
+++ src/app/courtier/courtier-routing.module.ts
@@ -5,6 +5,12 @@ import { DashbordComponent } from './dashbord/dashbord.component';
 import { ListeDossierComponent } from './liste-dossier/liste-dossier.component';
 import { ListeClientComponent } from './liste-client/liste-client.component';
 import { DetailComponent } from './detail/detail.component';
+import { MesdossierComponent } from './mesdossier/mesdossier.component';
+import { MonDossierComponent } from './mon-dossier/mon-dossier.component';
+import { InformationComponent } from './component/information/information.component';
+import { DetailCreditComponent } from './component/detail-credit/detail-credit.component';
+import { DetailClientComponent } from './component/detail-client/detail-client.component';
+import { DocumentComponent } from './component/document/document.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
@@ -14,8 +20,20 @@ const routes: Routes = [
     children: [
       { path: 'dashboard', component: DashbordComponent },
       { path: 'dossier', component: ListeDossierComponent },
+      { path: 'mes-dossier', component: MesdossierComponent },
       { path: 'client', component: ListeClientComponent },
       { path: 'detail', component: DetailComponent },
+      {
+        path: 'detail-dossier',
+        component: MonDossierComponent,
+        children: [
+          { path: '', redirectTo: 'information', pathMatch: 'full' },
+          { path: 'information', component: InformationComponent },
+          { path: 'detail-credit', component: DetailCreditComponent },
+          { path: 'detail-client', component: DetailClientComponent },
+          { path: 'document', component: DocumentComponent },
+        ],
+      },
     ],
   },
 ];
