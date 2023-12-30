7be6ea4b66c87c11866ff8c876b82063f049a542
mahrez <mahreztouzi@gmail.com>
2023-12-30 21:38:02 +0100

    simulation/ consomation/ result/ mensualité

--- src/app/simulation/result/result.component.ts
+++ src/app/simulation/result/result.component.ts
@@ -14,29 +14,49 @@ export class ResultComponent implements OnInit {
   apportInitial: number = 0;
   mensualite: string = '';
   revenue: string = '';
-
+  consomation: boolean = false;
   eligible: boolean = false; // Variable pour vérifier l'éligibilité
 
   ngOnInit(): void {
     const type = localStorage.getItem('financementType');
     const formImmobilierDataJson = localStorage.getItem('formImmobilierData');
+    const formConsomationData = localStorage.getItem('formConsomationData');
     console.log(formImmobilierDataJson);
+    if (type === 'immobilier') {
+      if (formImmobilierDataJson) {
+        // Convertissez la chaîne JSON en objet JavaScript
+        const formData = JSON.parse(formImmobilierDataJson);
 
-    if (formImmobilierDataJson) {
-      // Convertissez la chaîne JSON en objet JavaScript
-      const formData = JSON.parse(formImmobilierDataJson);
+        this.financement = formData.credit;
+        this.durer = formData.durer;
+        this.revenue = formData.revenueCumule;
+        this.habitation = formData.habitation;
+        this.apportInitial =
+          parseFloat(formData.habitation) - parseFloat(formData.credit);
+        // Appelez la méthode pour calculer la mensualité
+        this.calculerMensualite();
 
-      this.financement = formData.credit;
-      this.durer = formData.durer;
-      this.revenue = formData.revenueCumule;
-      this.habitation = formData.habitation;
-      this.apportInitial =
-        parseFloat(formData.habitation) - parseFloat(formData.credit);
-      // Appelez la méthode pour calculer la mensualité
-      this.calculerMensualite();
+        // Vérifiez l'éligibilité
+        this.verifierEligibilite();
+      }
+    } else if (type === 'consomation') {
+      this.consomation = true;
+      if (formConsomationData) {
+        // Convertissez la chaîne JSON en objet JavaScript
+        const formData = JSON.parse(formConsomationData);
 
-      // Vérifiez l'éligibilité
-      this.verifierEligibilite();
+        this.financement = formData.credit;
+        this.durer = formData.durer;
+        this.revenue = formData.revenueCumule;
+        this.habitation = formData.consommation;
+        this.apportInitial =
+          parseFloat(formData.consommation) - parseFloat(formData.credit);
+        // Appelez la méthode pour calculer la mensualité
+        this.calculerMensualiteConsomation();
+
+        // Vérifiez l'éligibilité
+        this.verifierEligibiliteConsomation();
+      }
     }
   }
 
@@ -51,6 +71,32 @@ export class ResultComponent implements OnInit {
     const financement = parseFloat(this.financement);
     const durer = parseFloat(this.durer);
     const interet = 0.065 / 12;
+
+    // Vérifiez si les valeurs sont valides
+    if (!isNaN(financement) && !isNaN(durer) && !isNaN(interet)) {
+      // Calcul de n (nombre de paiements)
+      const n = durer * 12;
+
+      // Calcul de la mensualité selon la formule
+      const mensualite =
+        (financement * interet * Math.pow(1 + interet, n)) /
+        (Math.pow(1 + interet, n) - 1);
+      console.log(mensualite);
+
+      // Affectez le résultat à la propriété mensualite
+      this.mensualite = mensualite.toString();
+    } else {
+      // Gérez le cas où les valeurs ne sont pas valides
+      console.error(
+        "Les valeurs de financement, durée ou taux d'intérêt ne sont pas valides."
+      );
+    }
+  }
+  calculerMensualiteConsomation() {
+    // Convertissez les chaînes en nombres
+    const financement = parseFloat(this.financement);
+    const durer = parseFloat(this.durer);
+    const interet = 0.085 / 12;
 
     // Vérifiez si les valeurs sont valides
     if (!isNaN(financement) && !isNaN(durer) && !isNaN(interet)) {
@@ -92,7 +138,26 @@ export class ResultComponent implements OnInit {
       }
     }
   }
+  verifierEligibiliteConsomation() {
+    // Convertissez les chaînes en nombres
+    const revenue = parseFloat(this.revenue);
+    const mensualite = parseFloat(this.mensualite);
 
+    if (!isNaN(revenue) && !isNaN(mensualite)) {
+      // Vérifiez l'éligibilité
+      if (revenue <= 80000) {
+        this.eligible = mensualite <= revenue * 0.4;
+      } else {
+        this.eligible = mensualite <= revenue * 0.5;
+      }
+      console.log('je suis ici', this.eligible);
+      // Si le client n'est pas éligible, ajustez le montant du financement
+      if (!this.eligible) {
+        this.ajusterMontantFinancementConsomation(revenue);
+      }
+    }
+  }
+  // pour immobilier
   ajusterMontantFinancement(revenue: number) {
     const interet = 0.065 / 12; // Taux d'intérêt mensuel
     const durer = parseFloat(this.durer);
@@ -117,6 +182,32 @@ export class ResultComponent implements OnInit {
       parseFloat(this.habitation) - parseFloat(this.financement);
     // Recalculer la mensualité avec le nouveau montant
     this.calculerMensualite();
+  }
+  // pour la consomation
+  ajusterMontantFinancementConsomation(revenue: number) {
+    const interet = 0.085 / 12; // Taux d'intérêt mensuel
+    const durer = parseFloat(this.durer);
+    const n = durer * 12; // Nombre de paiements
+
+    // Calculer la mensualité maximale admissible
+    let mensualiteMax;
+    if (revenue <= 80000) {
+      mensualiteMax = revenue * 0.4;
+    } else {
+      mensualiteMax = revenue * 0.5;
+    }
+
+    // Inverser la formule de la mensualité pour calculer le montant du financement
+    const nouveauMontant =
+      (mensualiteMax * (Math.pow(1 + interet, n) - 1)) /
+      (interet * Math.pow(1 + interet, n));
+
+    // Mettez à jour la valeur du financement
+    this.financement = nouveauMontant.toString();
+    this.apportInitial =
+      parseFloat(this.habitation) - parseFloat(this.financement);
+    // Recalculer la mensualité avec le nouveau montant
+    this.calculerMensualiteConsomation();
   }
 
   estDansPlageAcceptable(revenue: number): boolean {
