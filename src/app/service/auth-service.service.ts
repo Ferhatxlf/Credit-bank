607da951ea643019819221bd164c655553d96d78
Ferhat Khelf <ferhat.khelf@fgei.ummto.dz>
2024-01-02 12:07:58 +0100

    ajout de qlq services

---
+++ src/app/service/auth-service.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthServiceService {
+  private apiUrl = 'http://localhost:8080';
+  constructor(public http: HttpClient) {}
+
+  // inscription ...
+  register(client: any) {
+    return this.http.post(
+      `${this.apiUrl}/clients/subscribe/particulier`,
+      client
+    );
+  }
+
+  // Connexion ....
+  login(client: any) {
+    return this.http.post(`${this.apiUrl}/clients/login`, client);
+  }
+}
