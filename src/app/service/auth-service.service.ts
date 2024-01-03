09f39074070e882cf32bc1669a4930a2c8b52b50
Ferhat Khelf <ferhat.khelf@fgei.ummto.dz>
2024-01-03 18:35:04 +0100

    inscription

--- src/app/service/auth-service.service.ts
+++ src/app/service/auth-service.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -10,10 +10,17 @@ export class AuthServiceService {
   constructor(public http: HttpClient) {}
 
   // inscription ...
+
   register(client: any) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
     return this.http.post(
       `${this.apiUrl}/clients/subscribe/particulier`,
-      client
+      client,
+      httpOptions
     );
   }
 
