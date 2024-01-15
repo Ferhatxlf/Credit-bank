4bd22d72ba5406dc81fe812a91671abfd723df22
unknown <krimbelkacem.hamnache@fgei.ummto.dz>
2024-01-15 17:42:46 +0100

    new

--- src/app/service/websocket.service.ts
+++ src/app/service/websocket.service.ts
@@ -48,7 +48,7 @@ export class WebSocketService {
     if (this.socket && this.socket.connected) {
       this.disconnect();
     }
-    this.socket = io('https://illustrious-lovely-fedora.glitch.me');
+    this.socket = io('https://triangular-sly-plane.glitch.me');
 
     this.socket.on('connect_error', (error: any) => {
       console.error('WebSocket connection error:', error);
