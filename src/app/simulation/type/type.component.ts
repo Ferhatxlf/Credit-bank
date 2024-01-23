84150a01265618f7109a034c0ec250fd5c704238
mahrez <mahreztouzi@gmail.com>
2024-01-23 12:00:42 +0100

    image back ground home  optimized

--- src/app/simulation/type/type.component.ts
+++ src/app/simulation/type/type.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-type',
   templateUrl: './type.component.html',
   styleUrl: './type.component.css',
 })
-export class TypeComponent {
+export class TypeComponent implements OnInit {
   islamique: boolean = false;
+
+  ngOnInit(): void {
+    // suppression des données du formulaires
+    sessionStorage.removeItem('formImmobilierData');
+    sessionStorage.removeItem('formConsomationData');
+    sessionStorage.removeItem('formislamiqueData');
+  }
+
   setIslamiqueCheck() {
     this.islamique = !this.islamique;
     console.log("l'element est ", this.islamique);
