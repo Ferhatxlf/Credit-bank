d51c333f69e3c5e4c1b207b954dfa40c2d894384
unknown <krimbelkacem.hamnache@fgei.ummto.dz>
2024-01-27 11:52:50 +0100

    new

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,22 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'CreditDirect',
-  webDir: 'www',
+  appId: 'com.example.creditapp',
+  appName: 'CreditApp',
+  webDir: 'dist/credit-bank/browser',
   server: {
     androidScheme: 'https'
+  },
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      BackupWebStorage: 'none',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '3000'
+    }
   }
 };
 
