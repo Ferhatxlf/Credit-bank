3a89cf644dd9ab8e3d0e530063826e2429519bfb
unknown <krimbelkacem.hamnache@fgei.ummto.dz>
2024-02-02 18:02:05 +0100

    nnnnn

--- src/app/courtier/component/document/document.component.ts
+++ src/app/courtier/component/document/document.component.ts
@@ -52,7 +52,8 @@ export class DocumentComponent {
     // Open a URL in a new tab
 
     // Example usage
-    const targetUrl = `${this.apiUrl}/dossiers/downloadFile${name}`;
+    //const targetUrl = `${this.apiUrl}/dossiers/downloadFile${name}`;
+    const targetUrl = `${this.apiUrl}/dossiers/downloadFile/${this.id}/${name}`;
     this.openUrlInNewTab(targetUrl);
     
   }
