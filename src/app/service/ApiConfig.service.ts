5007f7272ac13e63fabcdd0f17e44fa7e3046daa
unknown <krimbelkacem.hamnache@fgei.ummto.dz>
2024-02-03 18:50:36 +0100

    nnnnn

--- src/app/service/ApiConfig.service.ts
+++ src/app/service/ApiConfig.service.ts
@@ -6,8 +6,10 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfigService {
-//  apiUrl = 'http://localhost:8000';
- apiUrl = 'https://observant-shock-production.up.railway.app';
+ //apiUrl = 'http://localhost:8000';
+ apiUrl = "https://creditdirectbackvdeploy-production.up.railway.app"
+ //apiUrl = 'https://observant-shock-production.up.railway.app';
+
 
   getApiUrl(): string {
     return this.apiUrl;
