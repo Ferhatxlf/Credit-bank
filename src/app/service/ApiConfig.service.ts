b0198ccfbbb82c2331f4898f5a7549d82aa4c251
Mah Rez <113209129+mahreztouzi@users.noreply.github.com>
2024-02-16 20:07:56 +0100

    Update ApiConfig.service.ts

--- src/app/service/ApiConfig.service.ts
+++ src/app/service/ApiConfig.service.ts
@@ -7,8 +7,8 @@ import { Injectable } from '@angular/core';
 })
 export class ApiConfigService {
  //apiUrl = 'http://localhost:8000';
- //apiUrl = 'https://creditdirectbackvdeploy-production.up.railway.app';
- apiUrl = 'https://credit-direct.onrender.com';
+ apiUrl = 'https://creditdirectbackvdeploy-production.up.railway.app';
+ // apiUrl = 'https://credit-direct.onrender.com';
 
 
   getApiUrl(): string {
