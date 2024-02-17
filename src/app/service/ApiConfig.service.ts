10c3b60dcae77f61b9b8a936fc31baf6b6195b47
unknown <krimbelkacem.hamnache@fgei.ummto.dz>
2024-02-17 03:58:04 +0100

    new

--- src/app/service/ApiConfig.service.ts
+++ src/app/service/ApiConfig.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 })
 export class ApiConfigService {
  //apiUrl = 'http://localhost:8000';
- apiUrl = 'https://creditdirectbackvdeploy-production.up.railway.app';
+ apiUrl = 'https://billowing-throne-production.up.railway.app';
 
 
 //apiUrl = 'https://creditdirect2.onrender.com';
